aca62c09cf01f8faed7446bd4d8bdd4939b47036
Nalberth <nalberthsantosr@gmail.com>
2022-03-11 18:10:37 -0300

    modularized menu and verify if user it's logged

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,12 +1,11 @@
+import verifyLogged from "./modules/verify-logged.js";
 import initUser from "./modules/user-data.js";
 import chartPopulate from "./modules/chart-populate.js";
 import initTablePopulation from "./modules/populate-table.js";
 import initAccordionMenu from "./modules/populate-menu.js";
-import handleAccordionMenuAnimation from "./modules/accordion-menu.js";
 
+verifyLogged();
 initUser();
 initTablePopulation();
 chartPopulate();
-
-await initAccordionMenu();
-handleAccordionMenuAnimation();
+initAccordionMenu();
