5a497dca74e5f3701c7a30a63d0cf2d70f0995f6
Nalberth <nalberthsantosr@gmail.com>
2022-03-11 18:06:44 -0300

    add save user when logged

--- scripts/authPage.js
+++ scripts/authPage.js
@@ -6,46 +6,56 @@ function initUser() {
 function getUserData(event) {
   event.preventDefault();
   const userEmail = document.getElementById("email").value;
-  const userName = document.getElementById("password").value;
+  const userPassword = document.getElementById("password").value;
 
-  if (userName !== "" && userEmail !== "") fetchLogin(userEmail, userName);
+  if (userPassword === "" && userEmail === "") return;
+  fetchLogin(userEmail, userPassword);
 }
 
-async function fetchLogin(userEmail, userName) {
+async function fetchLogin(email, password) {
   try {
     const URL = "https://test-final.b8one.academy/login";
     const response = await fetch(URL, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        email: userEmail,
-        password: userName,
-      }),
+      body: JSON.stringify({ email, password }),
     });
     if (!response.ok) throw new Error(response.message);
-    window.location.replace(window.location.origin);
+
+    saveUserLogged({ email });
+
+    window.location.replace("./dashboard.html");
   } catch (error) {
     handleUserNotFound();
   }
 }
 
 function handleUserNotFound() {
-  const labelWrong = document.querySelector(".card-login-form__input");
-  labelWrong.classList.add("card-login-form__input--wrong");
+  const inputWrong = document.querySelector(".card-login-form__input");
+  inputWrong.classList.add("card-login-form__input--wrong");
 
-  const inputWrong = document.querySelector(".card__input-wrong");
-  inputWrong.classList.add("card__input-wrong--active");
+  const feedbackHelperText = document.querySelector(".card__input-wrong");
+  feedbackHelperText.classList.add("card__input-wrong--active");
 
-  labelWrong.addEventListener("change", (e) => {
-    e.currentTarget.classList.remove("card-login-form__input--wrong");
+  inputWrong.addEventListener("change", ({ currentTarget }) => {
+    currentTarget.classList.remove("card-login-form__input--wrong");
     inputWrong.classList.remove("card__input-wrong--active");
   });
 }
 
-function saveTheCookies() {}
+function saveUserLogged(email) {
+  localStorage.setItem("user", JSON.stringify(email));
+}
 
 function showPassword() {
   const passwordButton = document.getElementById("password");
   if (passwordButton.type === "password") passwordButton.type = "text";
   else passwordButton.type = "password";
 }
+
+function verifyLogged() {
+  if (localStorage.getItem("user") !== null)
+    window.location.replace("./dashboard.html");
+}
+
+verifyLogged();
